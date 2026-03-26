803e4313b04d8bb8c2f55fc3d1b3670f57bb2db6
AdityaSR24 <adi.shashikant@gmail.com>
2026-03-26 18:39:45 +0530

    Added confirmation for transactions and QR display

--- franchise.py
+++ franchise.py
@@ -1,35 +1,45 @@
 import datetime
 import hashlib
 from PIL import Image
+import os
 
 class Franchise:
-  def __init__(self, f_name, f_acc_num, f_zone_code, f_pwd, f_balance):
+  def __init__(self, f_name, f_acc_num, f_zone_code, f_pwd, f_balance, fid):
     self.f_name = f_name
     self.f_acc_num = f_acc_num
     self.f_zone_code = f_zone_code
     self.f_pwd = f_pwd
     self.f_balance = f_balance
     self.f_time_acc_create = ((datetime.datetime.now()).strftime("%d-%m-%y %H:%M:%S")).split(" ")
+    self.vfid = None
+    self.qr_code = None
 
-    self.fid = self.generate_fid()
+    # FID generation - can be done here instead of in grid cuz it's just one line
+    raw = f_name + self.f_time_acc_create + f_pwd
+    self.fid = hashlib.sha3_256(raw.encode()).hexdigest()[:16]
 
-  def sha3_algo(self, message):
-    # can we just import it? or should we code it out?
-    try:
-      return hashlib.sha3_256(message.encode()).hexdigest()
-    except:
-      return "Could not hash"
-    # this returns hex code
+  def registration_with_grid(self, grid):
+    grid.register_franchise(self) # Implement register_franchise in grid
+    print(f"Franchise '{self.f_name}' registered with FID: {self.fid}")
 
-  def generate_fid(self):
-    message = f"{self.f_name}, {self.f_time_acc_create}, {self.f_pwd}"
-    ct = ((sha3_algo(message)).upper())[:16]
-    # unique to every station and shouldn’t be shared
-    return ct
+  def qr_display(self, qr_code, vfid):
+    self.qr_code = qr_code
+    self.vfid = vfid
 
-  def display_qrcode(self, qrcode_file_name):
-    img = Image.open(os.path.join("qrcodes", qrcode_file_name))
-    img.show()
+    if self.qr_code:
+      print(f"QR code received for franchise {self.f_name}")
+      qrcode_file_name = f"{self.f_name}_qr.png"
+      qrcode_file_name = os.path.join("qrcodes", qrcode_file_name)
+      self.qr_code.save(qrcode_file_name)
+
+      self.qr_code.show()
+
+  def confirmation(self, success, amount=0):
+    if success:
+      self.f_balance += amount
+      self.unlock_charging_cable(self, 1)
+    else:
+      print(f"Transaction for {self.f_name} rejected.")
 
   def unlock_charging_cable(self, flag):
     return True if (flag == 1) else False
